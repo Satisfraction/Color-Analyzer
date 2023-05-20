1b217bde7948872600e3ab9fcdda1972d66b7600
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-05-21 01:53:23 +0200

    Mayor Update
    switch from tkinter to pyqt5
    add new function "save_color" to save the average color as .txt file

--- ColorAnalyzer.py
+++ ColorAnalyzer.py
@@ -1,54 +1,114 @@
-import tkinter as tk
-from tkinter import filedialog
-from PIL import Image, ImageTk
+# Import necessary modules
+import sys
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QFileDialog
+from PIL import Image, ImageQt
+from PyQt5 import QtCore
 
-def analyze_color(image_path):
-    with Image.open(image_path) as img:
-        colors = img.getcolors(img.size[0] * img.size[1])
-        average_color = tuple(sum([c[0] * c[1][i] for c in colors for i in range(3)]) // sum([c[0] for c in colors]) for j in range(3))
-        return average_color
+# Create a class for our ColorAnalyzer widget
+class ColorAnalyzer(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-def show_image(image_path):
-    with Image.open(image_path) as img:
-        img.thumbnail((300, 300))
-        photo = ImageTk.PhotoImage(img)
-        label.config(image=photo)
-        label.image = photo
+    # Define the user interface
+    def initUI(self):
+        # Create a label
+        self.label1 = QtWidgets.QLabel(self)
+        self.label1.setText("<font color='blue'>Select an image:</font>")
+        self.label1.move(20, 20)
 
-def get_file():
-    file_path = filedialog.askopenfilename(title="Select Image File")
-    if file_path:
-        entry.delete(0, tk.END)
-        entry.insert(tk.END, file_path)
-        get_color()
+        # Create a button
+        self.button = QtWidgets.QPushButton(self)
+        self.button.setText("Open File")
+        self.button.move(20, 60)
+        self.button.setStyleSheet("background-color: #4CAF50; color: white;")
+        self.button.clicked.connect(self.get_file)
+        
+        # Create a save button
+        self.save_button = QtWidgets.QPushButton(self)
+        self.save_button.setText('Save Color')
+        self.save_button.move(100, 100)
+        self.save_button.setStyleSheet('background-color: #4CAF50; color: white;')
+        self.save_button.clicked.connect(self.save_color)
 
-def get_color():
-    image_path = entry.get()
-    color = analyze_color(image_path)
-    color_hex = '#{:02x}{:02x}{:02x}'.format(*color)
-    color_label.config(text=f"Average color: {color_hex}")
-    show_image(image_path)
+        # Create an entry box
+        self.entry = QtWidgets.QLineEdit(self)
+        self.entry.move(130, 60)
 
-root = tk.Tk()
-root.title("Color Analyzer")
-root.geometry("400x400")
+        # Create an analyze button
+        self.analyze_button = QtWidgets.QPushButton(self)
+        self.analyze_button.setText("Analyze color")
+        self.analyze_button.move(20, 100)
+        self.analyze_button.setStyleSheet("background-color: #4CAF50; color: white;")
+        self.analyze_button.clicked.connect(self.get_color)
 
-label1 = tk.Label(root, text="Select an image:")
-label1.pack()
+        # Create a label to display the average color
+        self.color_label = QtWidgets.QLabel(self)
+        self.color_label.setGeometry(20, 140, 400, 50)
+        self.color_label.setStyleSheet("background-color: #E91E63; color: white;")
 
-button = tk.Button(root, text="Open File", command=get_file)
-button.pack()
+        # Create a label to display the image
+        self.label = QtWidgets.QLabel(self)
+        self.label.setGeometry(20, 200, 360, 360)
+        self.label.setStyleSheet("background-color: white;")
 
-entry = tk.Entry(root)
-entry.pack()
+        # Set the window size and title
+        self.setGeometry(100, 100, 400, 600)
+        self.setWindowTitle('Color Analyzer')
 
-analyze_button = tk.Button(root, text="Analyze color", command=get_color)
-analyze_button.pack()
+        # Set the background color of the window
+        self.setStyleSheet("background-color: #F5F5F5;")
 
-color_label = tk.Label(root, text="")
-color_label.pack()
+        # Show the widget
+        self.show()
 
-label = tk.Label(root)
-label.pack()
+    # Define a function to get the file path of the selected image
+    def get_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Select Image File")
+        if file_path:
+            self.entry.clear()
+            self.entry.insert(file_path)
+            self.get_color()
 
-root.mainloop()
+    # Define a function to analyze the color of the selected image
+    def get_color(self):
+        image_path = self.entry.text()
+        color = self.analyze_color(image_path)
+        color_hex = '#{0:02x}{1:02x}{2:02x}'.format(*color)
+        self.color_label.setText('Average color: ' + color_hex)
+        self.show_image(image_path)
+
+    # Define a function to analyze the color of an image
+    def analyze_color(self, image_path):
+        with Image.open(image_path) as img:
+            colors = img.getcolors(img.size[0] * img.size[1])
+            average_color = tuple(sum([c[0] * c[1][i] for c in colors for i in range(3)]) // sum([c[0] for c in colors]) for j in range(3))
+            return average_color
+
+    # Define a function to display the selected image
+    def show_image(self, image_path):
+        with Image.open(image_path) as img:
+            img.thumbnail((600, 600))
+            qimage = ImageQt.ImageQt(img)
+            pixmap = QtGui.QPixmap.fromImage(qimage)
+            pixmap = pixmap.scaled(self.label.size() * 0.9, QtCore.Qt.KeepAspectRatio)
+            self.label.setPixmap(pixmap)
+            
+    def save_color(self):
+        color_hex = self.color_label.text().split(': ')[-1]
+        file_path, _ = QFileDialog.getSaveFileName(self, 'Save Color File', '', 'Text Files (*.txt)')
+        if file_path:
+            with open(file_path, 'w') as f:
+                f.write(color_hex)
+            
+# If this file is being run directly,
+if __name__ == '__main__':
+    # Create a new Qt application
+    app = QtWidgets.QApplication(sys.argv)
+
+    # Create an instance of the ColorAnalyzer widget
+    ex = ColorAnalyzer()
+
+    # Execute the application
+    sys.exit(app.exec_())
